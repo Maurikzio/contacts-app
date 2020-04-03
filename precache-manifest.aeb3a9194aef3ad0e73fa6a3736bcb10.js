2f6edc5e25426781d58d9ec3f666c0af3241d3e8
Maurikzio <m.morocho1196@gmail.com>
2020-04-04 00:32:55 +0300

    Updates

---
+++ precache-manifest.aeb3a9194aef3ad0e73fa6a3736bcb10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766b4e3fec7e0cf55cef3a45f520db9e",
+    "url": "/contacts-app/index.html"
+  },
+  {
+    "revision": "1fdd4d67e6288ef3f21a",
+    "url": "/contacts-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "55148841f78ca0cc57a6",
+    "url": "/contacts-app/static/js/2.5ab3276a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contacts-app/static/js/2.5ab3276a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fdd4d67e6288ef3f21a",
+    "url": "/contacts-app/static/js/main.a7b2598c.chunk.js"
+  },
+  {
+    "revision": "d97b419fa31246449b50",
+    "url": "/contacts-app/static/js/runtime-main.419f610c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contacts-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
