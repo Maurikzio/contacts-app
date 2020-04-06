faf9027458172ab06fe02c6ae37a0c3516a59d5f
Maurikzio <m.morocho1196@gmail.com>
2020-04-06 03:07:14 +0300

    Updates

--- precache-manifest.1fce93bd17e844f173b0df98a4c3233d.js
+++ precache-manifest.1fce93bd17e844f173b0df98a4c3233d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8114ceb9b2fd04679bccb7fdf25cb85b",
+    "revision": "a6504013880eb242a8aac45d87525824",
     "url": "/contacts-app/index.html"
   },
   {
-    "revision": "6a3c62d80b02fc399341",
-    "url": "/contacts-app/static/css/main.5f361e03.chunk.css"
+    "revision": "9696f9390824e95103cd",
+    "url": "/contacts-app/static/css/main.3e793b09.chunk.css"
   },
   {
-    "revision": "55148841f78ca0cc57a6",
-    "url": "/contacts-app/static/js/2.5ab3276a.chunk.js"
+    "revision": "79f0cf8f1e3648ce239f",
+    "url": "/contacts-app/static/js/2.a0ecb97d.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/contacts-app/static/js/2.5ab3276a.chunk.js.LICENSE.txt"
+    "url": "/contacts-app/static/js/2.a0ecb97d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6a3c62d80b02fc399341",
-    "url": "/contacts-app/static/js/main.4fc1cc16.chunk.js"
+    "revision": "9696f9390824e95103cd",
+    "url": "/contacts-app/static/js/main.dcfe34fa.chunk.js"
   },
   {
     "revision": "d97b419fa31246449b50",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contacts-app/precache-manifest.2e4692378ae226026cfcd0920fa541c5.js"
+  "/contacts-app/precache-manifest.1fce93bd17e844f173b0df98a4c3233d.js"
 );
 
 self.addEventListener('message', (event) => {
