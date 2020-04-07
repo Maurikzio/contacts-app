e85d7b31683292b060e6c410c6013f7e7f56c449
Maurikzio <m.morocho1196@gmail.com>
2020-04-07 03:00:10 +0300

    [fix] homework 16 advanced search

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -14,28 +14,32 @@ export default class Contacts extends Component{
     handleSearchChange = (event) => (
         this.setState({
             search: event.target.value,
-        }
+        },
         // () => this.handleAdvancedSearch(this.state.search)
         )
     )
-    
-    render() {
-        const query = this.state.contacts.filter(contact => Object.values(contact).find(value => value.includes(this.state.search)));
-        return(
-            <div className='container'>
-                <h2>Contacts</h2>
-                <div className='searchBar'>
-                    <p>&#9906;</p>
-                    <input value={this.state.search} onChange={this.handleSearchChange} placeholder="Search"/>
-                </div>
-                <ul className='contactList'>
-                {query.map(contact => (
-                        <Contact key={contact.firstName} {...contact}/>
-                    ))}
-                </ul>
-            </div>
-        )
+
+    advancedSearch = () => {
+        const query = this.state.contacts.filter(contact => 
+            contact.firstName.toLowerCase().includes(this.state.search.toLowerCase()) 
+            || contact.lastName.toLowerCase().includes(this.state.search.toLowerCase()) 
+            || contact.phone.includes(this.state.search.toLowerCase()))
+
+        return query.map(result => (
+            <Contact key={result.firstName} {...result}/>
+        ))
     }
+    
+    render = () => (
+        <div className='container'>
+            <h2>Contacts</h2>
+            <div className='searchBar'>
+                <p>&#9906;</p>
+                <input value={this.state.search} onChange={this.handleSearchChange} placeholder="Search"/>
+            </div>
+            <ul className='contactList'>{this.advancedSearch()}</ul>
+        </div>
+    )
 }
 
  {/* <ul className='contactList'>{[...this.state.advanced].map(item => (
