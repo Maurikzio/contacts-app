f170a4ce66c6be1002c6f069b94aa24ee1ee7c81
Maurikzio <m.morocho1196@gmail.com>
2020-04-08 04:04:27 +0300

    [add] Filter by gender tool added.

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -8,40 +8,69 @@ export default class Contacts extends Component{
         super(props);
         this.state = {
             contacts: props.data,
-            search: ''
+            search: '',
+            filters: [
+                {indicator: 'male', value: true},
+                {indicator: 'female', value: true},
+                {indicator: 'unknown', value: true},
+            ]
         }
     }
-    handleSearchChange = (event) => (
+    updateSearchValue = (event) => (
         this.setState({
             search: event.target.value,
-        },
-        // () => this.handleAdvancedSearch(this.state.search)
-        )
+        })
     )
 
-    advancedSearch = () => {
-        const query = this.state.contacts.filter(contact => 
-            contact.firstName.toLowerCase().includes(this.state.search.toLowerCase()) 
-            || contact.lastName.toLowerCase().includes(this.state.search.toLowerCase()) 
-            || contact.phone.includes(this.state.search.toLowerCase()))
-
-        return query.map(result => (
-            <Contact key={result.firstName} {...result}/>
-        ))
+    updateFilterValue = (event) => {
+        this.setState({
+            filters: this.state.filters.map(item => 
+                item.indicator === event.target.value 
+                ? {...item, value: !item.value} 
+                : item)
+        })
     }
-    
+
+    filterByGender = () => {
+        const genderToFind = this.state.filters.filter(item => item.value).map(item => item.indicator);
+        return this.state.contacts.filter(contact => genderToFind.includes(!(contact.gender) ? 'unknown' : contact.gender))
+    }
+
+    filterByPeronalInfo = (data) => {
+        return data.filter(item => 
+                item.firstName.toLowerCase().includes(this.state.search.toLowerCase()) 
+                || item.lastName.toLowerCase().includes(this.state.search.toLowerCase()) 
+                || item.phone.includes(this.state.search.toLowerCase()))
+    }
+
     render = () => (
         <div className='container'>
             <h2>Contacts</h2>
             <div className='searchBar'>
                 <p>&#9906;</p>
-                <input value={this.state.search} onChange={this.handleSearchChange} placeholder="Search"/>
+                <input value={this.state.search} onChange={this.updateSearchValue} placeholder="Search"/>
             </div>
-            <ul className='contactList'>{this.advancedSearch()}</ul>
+
+            <div className='genderFilter'>
+                <label>
+                    <input type='checkbox' value='male' checked={this.state.filters[0].value} onChange={this.updateFilterValue}/>
+                    M
+                </label>
+                <label>
+                    <input type='checkbox' value='female' checked={this.state.filters[1].value} onChange={this.updateFilterValue}/>
+                    F
+                </label>
+                <label>
+                    <input type='checkbox' value='unknown' checked={this.state.filters[2].value} onChange={this.updateFilterValue}/>
+                    NA
+                </label>
+            </div>
+
+            <ul className='contactList'>
+                {this.filterByPeronalInfo(this.filterByGender()).map(result => (
+                    <Contact key={result.firstName} {...result}/>
+                ))} 
+            </ul>
         </div>
     )
 }
-
- {/* <ul className='contactList'>{[...this.state.advanced].map(item => (
-                    <Contact key={item.firstName} data={item}/>
-                ))} */}
